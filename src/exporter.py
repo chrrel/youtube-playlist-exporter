25524e762075afdee99c5749714158de13689584
Christian <p-git@c5s.de>
2022-04-22 23:56:02 +0200

    cleaned up exporter and static files

--- src/exporter.py
+++ src/exporter.py
@@ -3,39 +3,43 @@ import json
 
 
 def playlists_to_html(playlists: list, filepath: str):
-
     playlists_list = ""
+    playlist_content = ""
 
-    for p in playlists:
-        print(p["meta"])
+    for playlist in playlists:
+        # Append name to list of playlists
         playlists_list += f"""
-            <a href="#{p["meta"]["Playlist-ID"]}">{p["meta"]["Titel"]}</a>
+        <a href="#{playlist["meta"]["Playlist-ID"]}">{playlist["meta"]["Titel"]}</a>
         """
 
-    html_content = ""
-    for playlist in playlists:
-        html_content += f"<div class='single-playlist' data-playlistid='{playlist['meta']['Playlist-ID']}'><h2>{playlist['meta']['Titel']}</h2>"
+        # Append content
+        playlist_content += f"""
+        <div class="single-playlist" data-playlistid="{playlist['meta']['Playlist-ID']}">
+            <h2>{playlist['meta']['Titel']}</h2>
+            {"".join(_video_to_html(video) for video in playlist["videos"])}
+        </div>
+        """
 
-        for video in playlist["videos"]:
-            data = video['metadata']
+    _save_to_html_file(playlist_content, playlists_list, filepath)
 
-            if not data.get('error') and data.get('videoThumbnails'):
-                html_content += f"""
-                <div class="video">
-                <img width="150" loading="lazy" src="{data.get('videoThumbnails')[4].get('url')}">
-                <div><h3><a href="https://www.youtube.com/watch?v={video['Video-ID']}">{data.get('title')}</a></h3>
-                <h4><a href="https://www.youtube.com{data.get('authorUrl')}">{data.get('author')}</a></h4></div>
-                </div>
-                """
-            else:
-                html_content += f"""
-                <div class="video">
-                <img width="150" height="84" style="background-color: #8E8C8C;">
-                <div><h3><a href="https://www.youtube.com/watch?v={video['Video-ID']}">{data.get('error')}</a></h3></div>
-                </div>
-                """
-        html_content += "</div>"
-    _save_to_html_file(html_content, playlists_list, filepath)
+
+def _video_to_html(video: dict) -> str:
+    data = video['metadata']
+    if not data.get('error') and data.get('videoThumbnails'):
+        return f"""
+        <div class="video">
+            <img width="150" loading="lazy" src="{data.get('videoThumbnails')[4].get('url')}">
+            <div><h3><a href="https://www.youtube.com/watch?v={video['Video-ID']}">{data.get('title')}</a></h3>
+            <h4><a href="https://www.youtube.com{data.get('authorUrl')}">{data.get('author')}</a></h4></div>
+        </div>
+        """
+    else:
+        return f"""
+        <div class="video">
+            <img width="150" height="84" style="background-color: #8E8C8C;">
+            <div><h3><a href="https://www.youtube.com/watch?v={video['Video-ID']}">{data.get('error')}</a></h3></div>
+        </div>
+        """
 
 
 def _esc(content) -> str:
@@ -57,7 +61,7 @@ def load_json(filepath: str):
         return json.load(file)
 
 
-def _save_to_html_file(html_content: str, playlists_list: str, filepath: str):
+def _save_to_html_file(playlist_content: str, playlists_list: str, filepath: str):
     # Use template as f-string and populate it with data
     js_code = _load_file_content("res/main.js")
     css_code = _load_file_content("res/styles.css")
