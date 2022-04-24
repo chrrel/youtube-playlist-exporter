166a957d7c1c3ef2c9d0228af0294a35445ae838
Christian <p-git@c5s.de>
2022-04-24 21:01:50 +0200

    use template strings instead of fstrings

--- src/exporter.py
+++ src/exporter.py
@@ -2,6 +2,8 @@ import html
 import json
 import datetime
 
+from string import Template
+
 
 def playlists_to_html(playlists: list, filepath: str):
     playlists_list = ""
@@ -9,44 +11,56 @@ def playlists_to_html(playlists: list, filepath: str):
 
     for playlist in playlists:
         # Append name to list of playlists
-        playlists_list += f"""
-        <a href="#{playlist.get("id")}">{playlist.get("title")}</a>
-        """
+        t = Template('<a href="#$playlist_id">$playlist_title</a>')
+        playlists_list += t.substitute(playlist_id=playlist.get("id"), playlist_title=playlist.get("title"))
 
         # Append content
-        playlist_content += f"""
-        <div class="single-playlist" data-playlistid="{playlist.get("id")}">
-            <h2>{playlist.get("title")}</h2>
-            <p>{playlist.get("description")}</p>
-            {"".join(_video_to_html(video) for video in playlist["videos"])}
-        </div>
-        """
+        t = Template('<div class="single-playlist" data-playlistid="$id"><h2>$title</h2><p>$description</p>$videos</div>')
+        playlist_content += t.substitute(
+            id=playlist.get("id"),
+            title=playlist.get("title"),
+            description=playlist.get("description"),
+            videos="".join(_video_to_html(video) for video in playlist["videos"]),
+        )
 
     _save_to_html_file(playlist_content, playlists_list, filepath)
 
 
 def _video_to_html(video: dict) -> str:
     data = video["metadata"]
-    length = str(datetime.timedelta(seconds=int(data.get("lengthSeconds"))))
-    published = datetime.datetime.utcfromtimestamp(data.get("published")).strftime("%d.%m.%Y")
-    if data and not data.get("error") and data.get("videoThumbnails"):
-        return f"""
+
+    video_template = f"""
         <div class="video">
             <div class="video-thumbnail-container">
-                <img width="150" loading="lazy" src="{data.get("videoThumbnails")[4].get("url")}">
-                <span class="video-duration">{length}</span>
+                <img width="150" loading="lazy" src="$thumbnail">
+                <span class="video-duration">$length</span>
             </div>
-            <div><h3><a href="https://www.youtube.com/watch?v={video.get("id")}">{data.get("title")}</a></h3>
-            <h4><a href="https://www.youtube.com{data.get("authorUrl")}">{data.get("author")}</a>, {published}</h4></div>
+            <div><h3><a href="https://www.youtube.com/watch?v=$video_id">$title</a></h3>
+            <h4><a href="https://www.youtube.com$author_url">$author</a> $published</h4></div>
         </div>
         """
+    t = Template(video_template)
+
+    if data and not data.get("error") and data.get("videoThumbnails"):
+        return t.substitute(
+            thumbnail=data.get("videoThumbnails")[4].get("url"),
+            length=str(datetime.timedelta(seconds=int(data.get("lengthSeconds")))),
+            video_id=video.get("id"),
+            title=data.get("title"),
+            author_url=data.get("authorUrl"),
+            author=data.get("author"),
+            published=", " + datetime.datetime.utcfromtimestamp(data.get("published")).strftime("%d.%m.%Y")
+        )
     else:
-        return f"""
-        <div class="video">
-            <img width="150" height="84" style="background-color: #8E8C8C;">
-            <div><h3><a href="https://www.youtube.com/watch?v={video.get("id")}">{data.get("error")}</a></h3></div>
-        </div>
-        """
+        return t.substitute(
+            thumbnail="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==",
+            length="0",
+            video_id=video.get("id"),
+            title=data.get("error"),
+            author_url="",
+            author="",
+            published=""
+        )
 
 
 def _esc(content) -> str:
